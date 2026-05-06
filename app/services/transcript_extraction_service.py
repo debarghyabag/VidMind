7dc1fbf4c6356239924c046c1dc45429f93ecc47
Debarghya <07debarghya@gmail.com>
2026-05-06 22:36:38 +0530

    transcript saved locally as json.

--- app/services/transcript_extraction_service.py
+++ app/services/transcript_extraction_service.py
@@ -1,10 +1,16 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+import json
+from pathlib import Path
+from app.utils.paths import FILE_STORAGE_PATH
+
 
 from app.models.schemas.transcript_schema import (
     TranscriptSegment,
     TranscriptResponse,
     ErrorResponse
 )
+
+FILE_STORAGE_PATH.mkdir(parents=True, exist_ok=True)
 
 
 def extract_transcript(video_id: str):
@@ -13,7 +19,9 @@ def extract_transcript(video_id: str):
 
         api = YouTubeTranscriptApi()
 
-        transcript_data = api.fetch(video_id)
+        transcript_list = api.list(video_id)
+        first_transcript = next(iter(transcript_list))
+        transcript_data = first_transcript.fetch()
 
         segments = [
             TranscriptSegment(
@@ -23,12 +31,17 @@ def extract_transcript(video_id: str):
             )
             for item in transcript_data
         ]
-
-        return TranscriptResponse(
+        response = TranscriptResponse(
             video_id=video_id,
             transcript=segments
         )
+        file_path = FILE_STORAGE_PATH / f"{video_id}.json"
+        with open(file_path, "w") as f:
+            json.dump(response.model_dump(), f, indent=4)
 
+        return response
+    
+    
     except Exception as e:
         return ErrorResponse(
             error=str(e)
--- app/utils/paths.py
+++ app/utils/paths.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 YOUTUBE_ID_PATH = BASE_DIR / "app/utils/youtube.py"
+FILE_STORAGE_PATH = BASE_DIR / "app/data_storage"
